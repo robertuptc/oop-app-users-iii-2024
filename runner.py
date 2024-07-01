554e2a360a6b2cc62797a3271ecba13b96dadb35
robertuptc <robertuptc@gmail.com>
2024-07-01 10:34:38 -0400

    premium user finished

--- runner.py
+++ runner.py
@@ -2,17 +2,18 @@ from users.User import User
 from users.FreeUser import FreeUser
 # from users.PremiumUser import PremiumUser
 
-# robert = User('robert', 'robert@mail.com')
-# print(robert.posts)
-# robert.add_post('first post')
-# print(robert.posts)
+premium_user = User('robert', 'robert@mail.com')
+print(premium_user.posts)
+premium_user.add_post('first post')
+print(premium_user.posts)
 
-free_user_bob = FreeUser('bob', 'bob@gmail.com')
-print(free_user_bob)
-free_user_bob.add_post('1st post')
-print(free_user_bob.posts)
-free_user_bob.add_post('2st post')
-print(free_user_bob.posts)
-free_user_bob.add_post('3st post')
-free_user_bob.deleting_post(1)
-print(free_user_bob.posts)
+
+# free_user_bob = FreeUser('bob', 'bob@gmail.com')
+# print(free_user_bob)
+# free_user_bob.add_post('1st post')
+# print(free_user_bob.posts)
+# free_user_bob.add_post('2st post')
+# print(free_user_bob.posts)
+# free_user_bob.add_post('3st post')
+# free_user_bob.deleting_post(1)
+# print(free_user_bob.posts)
--- users/PremiumUser.py
+++ users/PremiumUser.py
@@ -1,1 +1,5 @@
-# Your PremiumUser class goes here
+from users.User import User
+
+class PremiumUser(User):
+    def __init__(self, name, email):
+        super().__init__(name, email)
